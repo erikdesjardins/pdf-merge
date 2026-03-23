87524a78c3cb3a7953787e100a737d27f2f52e7a
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2026-03-22 21:24:35 -0400

    first argument is not the exe name

--- PdfMerge.cs
+++ PdfMerge.cs
@@ -4,10 +4,8 @@ using System.Linq;
 using iText.Kernel.Pdf;
 using iText.Kernel.Utils;
 
-var files = args.Skip(1);
-
 var merger = new PdfMerger(new PdfDocument(new PdfWriter(Console.OpenStandardOutput())));
-foreach (var fileName in files)
+foreach (var fileName in args)
 {
     using var file = new PdfDocument(new PdfReader(fileName));
     merger.Merge(file, 1, file.GetNumberOfPages());
